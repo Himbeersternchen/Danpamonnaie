e2298f1fb0afe37370112908a4f17a10e40ca5c7
Himbeersternchen <luffy0325@outlook.de>
2026-04-29 00:16:38 +0200

    add github pages

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,23 +3,26 @@ import App from "./App";
 import { Dashboard } from "./features/dashboard/ui/container/Dashboard";
 import { Tutorial } from "./features/tutorial/Tutorial";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Navigate to="/dashboard" replace />,
-      },
-      {
-        path: "dashboard",
-        element: <Dashboard />,
-      },
-      {
-        path: "tutorial",
-        element: <Tutorial />,
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <Navigate to="/dashboard" replace />,
+        },
+        {
+          path: "dashboard",
+          element: <Dashboard />,
+        },
+        {
+          path: "tutorial",
+          element: <Tutorial />,
+        },
+      ],
+    },
+  ],
+  { basename: import.meta.env.BASE_URL }
+);
